cbd8615298167e42f2f71f1703eff3fdcfb243e0
Marcial S <ckomarcial@gmail.com>
2026-07-07 16:55:54 -0300

    mais uma linha

--- src/versiongit/VersionGit.java
+++ src/versiongit/VersionGit.java
@@ -16,6 +16,7 @@ public class VersionGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
+        System.out.println("2026");
     }
     
 }
